d5655f45d3c3938ffc83f531cf6d45b4c5ce27da
zxl hhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2026-05-03 11:10:52 +0800

    hy2: update to 2.8.2

--- hy2.sh
+++ hy2.sh
@@ -6,7 +6,7 @@
 set -e
 
 # ---------- 默认配置 ----------
-HYSTERIA_VERSION="v2.6.5"
+HYSTERIA_VERSION="v2.8.2"
 DEFAULT_PORT=22222         # 自适应端口
 AUTH_PASSWORD="ieshare2025"   # 建议修改为复杂密码
 CERT_FILE="cert.pem"
